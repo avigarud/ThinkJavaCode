13db0decde670f56266ab3396484178e546ad05c
Avinash v Garud <41214994+avigarud@users.noreply.github.com>
2018-07-26 10:17:49 -0500

    Create method exercise

--- ch01/Hello.java
+++ ch01/Hello.java
@@ -1,8 +1,12 @@
 public class Hello {
 
     public static void main(String[] args) {
+
+
         // generate some simple output
         System.out.println("Hello, World!");
+        //this is comments
+        System.out.println("Hi there again ");
     }
 
 }
