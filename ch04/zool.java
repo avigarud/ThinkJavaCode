8719f8af39a849d7344b436e8f5cf03fb5108d0f
Avinash v Garud <41214994+avigarud@users.noreply.github.com>
2018-07-26 10:13:16 -0500

    Create method exercise

---
+++ ch04/zool.java
@@ -0,0 +1,16 @@
+public class zool
+{
+    public static void main(String[] args)
+    {
+         int number=11;
+         String one="jack";
+         String two="pune";
+        zool(number,one,two);
+    }
+
+    public static void zool(int number,String one,String two)
+    {
+        System.out.println("output"+number+""+two+""+ one );
+
+    }
+}
